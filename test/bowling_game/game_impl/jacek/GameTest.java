729d08ff006725bb8d0ae8a691306cf3f00b17dd
JacekX Lisinski <jacekx.lisinski@intel.com>
2018-03-25 14:09:03 +0200

    Test01: canCreateGame

--- test/bowling_game/game_impl/jacek/GameTest.java
+++ test/bowling_game/game_impl/jacek/GameTest.java
@@ -1,9 +1,21 @@
 package bowling_game.game_impl.jacek;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GameTest {
 
+    private GameByJacek sut;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new GameByJacek();
+    }
+
+    @Test
+    public void canCreateGame() {
+        assertNotNull(sut);
+    }
 }
